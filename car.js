921ddf1b68fe8882ea64513f6b74e68cdf4fc9a7
imaan yazdan <imaan.yazdan@outlook.com>
2022-02-01 11:28:18 +0000

    javascript

---
+++ car.js
@@ -0,0 +1,17 @@
+'use strict';
+
+console.log("imz")
+
+console.info("F")
+
+console.warn("London")
+
+console.error("Taurus")
+
+let favecar = "Chevrolet";
+let favemodel = "idk";
+console.log("my fave car is: " + favecar + " and its model is: " + favemodel);
+
+//template literals = console.log('my fav car is ${favecar} and the model is ${favemodel}`);
+
+console.log("%c This text is pink, comic-sans font, in bold with a black background","color: pink; font-size:25px; font-family:comic-sans; font-style: Bold; background-color: black;padding: 5px");
---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+//simple console.log() statement
+
+console.log("Hello World");
+
+let str1 = "Welcome to JavaScript";
+
+console.log(str1);
+
+console.info(str1);
+
+console.warn(str1);
+
+console.error(str1);
+
+let car = "Audi A5 ";
+let myObj = { str: "Just keep logging you are nearly there ", id:48576 }
+console.info("My first car was a ", car, ". The object is: ", myObj);
+
+let myTxt = "This could be useful!";
+
+console.log("this is a %c a message with some CSS."+myTxt,"color: yellow; font-style: italic; background-color: blue;padding: 2px");
